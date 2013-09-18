6b61bcc6bf92788006547cab08f6f29ebf432701
Edalzebu <edwin_zelaya5@hotmail.com>
2013-09-18 14:23:47 -0600

    Files Api Working on theory

--- MiniDropbox.Web/Controllers/API/FilesController.cs
+++ MiniDropbox.Web/Controllers/API/FilesController.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web;
 using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+using Amazon.S3.Model;
 using AutoMapper;
 using MiniDropbox.Domain;
 using MiniDropbox.Domain.Entities;
 using MiniDropbox.Web.Models;
 using MiniDropbox.Web.Models.Api;
 using MiniDropbox.Domain.Services;
+using File = MiniDropbox.Domain.File;
 
 namespace MiniDropbox.Web.Controllers.API
 {
@@ -25,55 +31,53 @@ namespace MiniDropbox.Web.Controllers.API
             _writeOnlyRepository = writeOnlyRepository;
         }
 
-        // GET api/files     
-        public List<DiskContentModel> Get([FromUri]string token)
-        {
-            var account = CheckPermissions(token);
-            var userContent = new List<DiskContentModel>();
-            if (CheckCuenta(account))
-            {
-                userContent=ListRootFolder(account);
-            }
-            return userContent;
-        }
-
         // GET api/files/5
-        public List<DiskContentModel> Get([FromBody]string currentPath, [FromUri]string token)
+        public string Get([FromUri]int Id, [FromUri]string token)
         {
             var account = CheckPermissions(token);
             if (CheckCuenta(account))
             {
-                return ListFolder(currentPath, account);
+                return JsonConvert.SerializeObject(DownloadFile(Id, account)); // HttpResponnsemessage
                 //Mandar el File con ese ID si le pertenece
             }
             return null;
         }
 
-        public bool CreateFolder()
-        {
-            return false;
-        }
+        
 
         // POST api/files
-        public string Post([FromBody]string dirAt, string name) // Para crear una carpeta
+        public bool Post([FromBody]UploadFileModel model, [FromUri] string token) // Para subir un archivo
         {
-            if (CreateFolder())
+
+            var account = CheckPermissions(token);
+            if (model != null)
             {
-                return name+" ha sido creado.";
+                if (CheckCuenta(account))
+                {
+                    if (UploadFile(model))
+                    {
+                        return true;
+                    }
+                }
             }
-            return "test";
+            
+            return false;
 
         }
 
-        // PUT api/files/5
-        public void Put(int id, [FromBody]string value)
+       // DELETE api/files/5
+        public bool Delete([FromUri]int id, [FromUri]string token)
         {
-        }
-
-        // DELETE api/files/5
-        public void Delete(int id)
-        {
-
+            var account = CheckPermissions(token);
+            if (CheckCuenta(account))
+            {
+                if (DeleteFile(id, account))
+                {
+                    return true;
+                }
+                //Mandar el File con ese ID si le pertenece
+            }
+            return false;
         }
 
         private Account CheckPermissions(string token) // Hace un check si el token existe, si existe devuelve una cuenta, sino null;
@@ -98,57 +102,100 @@ namespace MiniDropbox.Web.Controllers.API
            
         }
 
-        private List<DiskContentModel> ListRootFolder(Account account)
+        private HttpResponseMessage DownloadFile(int fileId, Account userData)
         {
-            var userData = _readOnlyRepository.First<Account>(x => x.EMail == account.EMail);
-            var userContent = new List<DiskContentModel>();
-
-            var actualFolder = "";
+            var fileData = _readOnlyRepository.First<File>(x => x.Id == fileId);
+            var objectRequest = new GetObjectRequest { BucketName = userData.BucketName, Key = fileData.Url + fileData.Name };
+            var file = AWSClient.GetObject(objectRequest);
+            var byteArray = new byte[file.ContentLength];
+            file.ResponseStream.Read(byteArray, 0, (int)file.ContentLength);
             
-            var userFiles = userData.Files;
-
-            
-
-            foreach (var file in userFiles)
-            {
-                if (file == null)
-                    continue;
-
-                var fileFolderArray = file.Url.Split('/');
-                var fileFolder = fileFolderArray.Length > 1 ? fileFolderArray[fileFolderArray.Length - 2] : fileFolderArray.FirstOrDefault();
-
-                if (!file.IsArchived && fileFolder.Equals(actualFolder) && !string.Equals(file.Name, actualFolder))
-                    userContent.Add(Mapper.Map<DiskContentModel>(file));
-            }
-            //Mandar todos los archivos de la cuenta
-            return userContent;
+            HttpResponseMessage response = new HttpResponseMessage();
+            response.StatusCode = HttpStatusCode.OK;
+            response.Content = new StreamContent(file);
+            return response;
         }
 
-        private List<DiskContentModel> ListFolder(string currentPath, Account account)
+        private bool DeleteFile(int fileId , Account userData)
         {
-            var userData = _readOnlyRepository.First<Account>(x => x.EMail == account.EMail);
-            var userContent = new List<DiskContentModel>();
+            
+            var fileToDelete = userData.Files.FirstOrDefault(f => f.Id == fileId);
 
-            var actualFolder = currentPath;
-
-            var userFiles = userData.Files;
-
-
-
-            foreach (var file in userFiles)
+            if (fileToDelete != null)
             {
-                if (file == null)
-                    continue;
+                if (!fileToDelete.IsDirectory)
+                {
+                    var deleteRequest = new DeleteObjectRequest { BucketName = userData.BucketName, Key = fileToDelete.Url + fileToDelete.Name };
+                    AWSClient.DeleteObject(deleteRequest);
+                }
 
-                var fileFolderArray = file.Url.Split('/');
-                var fileFolder = fileFolderArray.Length > 1 ? fileFolderArray[fileFolderArray.Length - 2] : fileFolderArray.FirstOrDefault();
+                //Borrar carpetas, mandar mensaje de confirmacion para eliminar cuando esta vacia y cuando contiene algun archivo
 
-                if (!file.IsArchived && fileFolder.Equals(actualFolder) && !string.Equals(file.Name, actualFolder))
-                    userContent.Add(Mapper.Map<DiskContentModel>(file));
+                //System.IO.File.Delete(fileToDelete.Url+fileToDelete.Name);
+
+                fileToDelete.IsArchived = true;
+
+                _writeOnlyRepository.Update(userData);
+                return true;
             }
-            //Mandar todos los archivos de la cuenta
-            return userContent;
+            return false;
+        }
+        private bool UploadFile(UploadFileModel model)
+        {
+            var fileControl = model.file;
+            if (fileControl == null)
+            {
+                return false;
+            }
 
+            var fileSize = fileControl.ContentLength;
+
+            if (fileSize > 10485760)
+            {
+                return false;
+            }
+
+            var userData = _readOnlyRepository.First<Account>(x => x.EMail == User.Identity.Name);
+            var actualPath = model.CurrentPath;
+            var fileName = Path.GetFileName(fileControl.FileName);
+
+            
+
+            if (userData.Files.Count(l => l.Name == fileName) > 0)//Actualizar Info Archivo
+            {
+                var bddInfo = userData.Files.FirstOrDefault(f => f.Name == fileName);
+                bddInfo.ModifiedDate = DateTime.Now;
+                bddInfo.Type = fileControl.ContentType;
+                bddInfo.FileSize = fileSize;
+                _writeOnlyRepository.Update(bddInfo);
+            }
+            else
+            {
+                userData.Files.Add(new File
+                {
+                    Name = fileName,
+                    CreatedDate = DateTime.Now,
+                    ModifiedDate = DateTime.Now,
+                    FileSize = fileSize,
+                    Type = fileControl.ContentType,
+                    Url = actualPath,
+                    IsArchived = false,
+                    IsDirectory = false
+                });
+                _writeOnlyRepository.Update(userData);
+            }
+
+            //fileControl.SaveAs(path);
+            var putObjectRequest = new PutObjectRequest
+            {
+                BucketName = userData.BucketName,
+                Key = actualPath + fileName,
+                InputStream = fileControl.InputStream
+            };
+
+            AWSClient.PutObject(putObjectRequest);
+
+            return true;
         }
     }
 }
