50c53f6699a438de251ce3c50114e3d2b9d794ef
canales16 <vari_14@hotmail.com>
2013-09-18 20:20:22 -0600

    Primer Commit

---
+++ MiniDropbox.Web/Controllers/AccountSignUpController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+using AutoMapper;
+using BootstrapMvcSample.Controllers;
+using MiniDropbox.Domain;
+using MiniDropbox.Domain.Services;
+using MiniDropbox.Web.Models;
+using File = System.IO.File;
+
+
+namespace MiniDropbox.Web.Controllers
+{
+    public class AccountSignUpController : BootstrapBaseController
+    {
+        private readonly IReadOnlyRepository _readOnlyRepository;
+        private readonly IWriteOnlyRepository _writeOnlyRepository;
+
+        public AccountSignUpController( IWriteOnlyRepository writeOnlyRepository, IReadOnlyRepository readOnlyRepository)
+        {
+            _readOnlyRepository = readOnlyRepository;
+            _writeOnlyRepository = writeOnlyRepository;
+        }
+        
+        [HttpGet]
+        public ActionResult AccountSignUp(long token)
+        {
+            Session["userReferralId"] = token;
+            
+            return View(new AccountSignUpModel());
+        }
+       
+        public ActionResult Cancelar()
+        {
+            return RedirectToAction("LogIn","Account");
+        }
+
+        [HttpPost]
+        public ActionResult AccountSignUp(AccountSignUpModel model)
+        {
+            var result = _readOnlyRepository.Query<Account>(a=>a.EMail==model.EMail);
+
+            if (result.Any())
+            {
+                Error("Email account is already registered in this site!!!");
+                return View(model);
+            }
+
+            var account = Mapper.Map<Account>(model);
+            account.IsArchived = false;
+            account.IsAdmin = false;
+            account.IsBlocked = false;
+            account.SpaceLimit = 500;
+            account.Password = EncriptacionMD5.Encriptar(model.Password);
+
+            //var account = new Account
+            //{
+            //    Name = accountModel.Name,
+            //    LastName = accountModel.LastName,
+            //    EMail = accountModel.EMail,
+            //    IsArchived = false,
+            //    IsBlocked = false,
+            //    SpaceLimit = 500,
+            //    UsedSpace = 0,
+            //    Password = EncriptacionMD5.Encriptar(accountModel.Password) 
+            //};
+            //account.AddRole(new Role{Name = "User",IsArchived = false});
+
+           var createdAccount= _writeOnlyRepository.Create(account);
+
+            var token = Convert.ToInt64(Session["userReferralId"]);
+
+            if (token != 0)
+            {
+                var userReferring = _readOnlyRepository.GetById<Account>(token);
+                userReferring.Referrals.Add(createdAccount);
+                _writeOnlyRepository.Update(userReferring);
+            }
+
+            var serverFolderPath = Server.MapPath("~/App_Data/UploadedFiles/" + account.EMail);
+            Directory.CreateDirectory(serverFolderPath);
+
+            var sharedDirectory =serverFolderPath + "/Shared";
+            Directory.CreateDirectory(sharedDirectory);
+
+            if (createdAccount.Files == null)
+            {
+                createdAccount.Files= new List<Domain.File>();
+            }
+
+            createdAccount.Files.Add(new Domain.File
+            {
+                CreatedDate = DateTime.Now,
+                FileSize = 0,
+                IsArchived = false,
+                IsDirectory = true,
+                Name = "Shared",
+                Url = serverFolderPath,
+                Type = "",
+                ModifiedDate = DateTime.Now
+            });
+
+            _writeOnlyRepository.Update(createdAccount);
+            return Cancelar();
+        }
+
+    }
+}
---
+++ MiniDropbox.Web/Infrastructure/AutoMapperConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using AutoMapper;
+using MiniDropbox.Domain;
+using MiniDropbox.Web.Controllers;
+using MiniDropbox.Web.Models;
+using Ninject.Modules;
+
+namespace MiniDropbox.Web.Infrastructure
+{
+    public class AutoMapperConfiguration
+    {
+        public static void Configure()
+        {
+            Mapper.CreateMap<AccountSignUpModel, Account>();
+            Mapper.CreateMap<Account, AccountSignUpModel>();
+            
+            Mapper.CreateMap<Account, AccountProfileModel>();
+            Mapper.CreateMap<AccountProfileModel, Account>();
+            
+            Mapper.CreateMap<Account, RegisteredUsersListModel>();
+            Mapper.CreateMap<RegisteredUsersListModel, Account>();
+            
+            Mapper.CreateMap<Account, ChangeUserSpaceLimitModel>();
+            Mapper.CreateMap<ChangeUserSpaceLimitModel, Account>();
+
+            Mapper.CreateMap<File,DiskContentModel>();
+            Mapper.CreateMap<DiskContentModel, File>();
+
+            Mapper.CreateMap<Package, PackageModel>();
+            Mapper.CreateMap<PackageModel, Package>();
+
+            Mapper.CreateMap<Package,CreateEditPackageController>();
+            Mapper.CreateMap<CreateEditPackageController, Package>();
+
+            Mapper.CreateMap<File, FileSearchResult>();
+            Mapper.CreateMap<FileSearchResult, File>();
+
+        }
+    }
+}
